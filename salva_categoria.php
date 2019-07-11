ff15cf9a2050d75196cee726e63dcfb6a1beef8e
YAN RODRIGUES DIAS <yan.rdias@senacsp.edu.br>
2019-07-10 22:18:28 -0300

    alteracoes_bd

--- salva_categoria.php
+++ salva_categoria.php
@@ -1,11 +1,15 @@
 
 <?php include("cabecalho.php"); ?>
+<?php include("bd.php") ?>
 <?php
 $nome = $_POST["nome"];
 $descricao = $_POST["descricao"];
-if(mysqli_query($conexao, $query)){
-echo "<p class='alert-success'> A categoria $nome - $descricao, foi cadastrada</p>";
-}else{
+
+  $query = "insert into PRODUTO (NOME,PRECO) values ('{$nome}','{$descricao}')";
+
+    if(mysqli_query($conexao, $query)){
+      echo "<p class='alert-success'> A categoria $nome - $descricao, foi cadastrada</p>";
+}   else{
 echo "<p class='alert-success'>Ocorreu um erro ao cadastrar categoria.</p>";
 }
 ?>
--- salva_produto.php
+++ salva_produto.php
@@ -1,10 +1,11 @@
 <?php include("cabecalho.php"); ?>
+<?php include("bd.php") ?>
 <?php
   $nome = $_POST["nome"];
   $preco = $_POST["preco"];
   $query = "insert into PRODUTO (NOME, PRECO) values ('{$nome}', {$preco})";
-  $conexao = mysqli_connect('localhost', 'root', '', 'ecommerce');
-  if(mysqli_query($conexao, $query)){
+
+    if(mysqli_query($conexao, $query)){
     echo "<p class='alert-success'> O produto $nome, no valor $preco foi cadastrado!</p>";
   } else {
     echo "<p class='alert-danger'> Ocorreu um problema ao cadastrar o produto.</p>";
