134fe3a869e72ce4954e712885e67735f567bc17
YAN RODRIGUES DIAS <yan.rdias@senacsp.edu.br>
2019-07-10 21:34:27 -0300

    Alteracao salva_produto para novo script

--- salva_produto.php
+++ salva_produto.php
@@ -1,9 +1,17 @@
+<?php include ("cabecalho.php"); ?>
 <?php
 
   $nome = $_POST["nome"];
   $preco = $_POST["preco"];
 
-    echo "O produto $nome, no valor $preco foi cadstradado!";
+    $query = "insert into PRODUTO (nome, preco) values ('{$nome}', {$preco})";
 
+    $conexao - mysqli_connect ('localhost','root','','ecommerce');
 
- ?>
+    if (mysqli_query($conexao, $query)) {
+        echo "<p class='alert-sucess'> O produto $nome, no valor $preco foi cadastrado!</p>";
+    } else {
+      echo "<p class='alert-danger'> Ocorreu um erro ao cadastrar o produto.</p>";
+    }
+?>
+ <?php include("rodape.php"); ?>
