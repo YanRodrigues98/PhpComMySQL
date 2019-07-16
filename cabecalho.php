08c35dd99b40de0b15c9ca038ae2fb50b2c587d4
YAN RODRIGUES DIAS <yan.rdias@senacsp.edu.br>
2019-07-15 22:16:12 -0300

    Mudancas de stylo e formas da pagina

--- cabecalho.php
+++ cabecalho.php
@@ -2,19 +2,19 @@
   <head>
     <title>Loja Virtual</title>
     <meta charset="utf-8">
-    <link href="css/bootstrap.css" rel="stylesheet"/>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"/>
     <link href="css/ecommerce.css" rel="stylesheet"/>
   </head>
   <body>
     <div class= "navbar navbar-inverse navbar-fixed-top">
       <div class="container">
-        <div class="nav navbar-nav">
-          <a href="index.php" class="navbar=brand">Minha Loja</a>
-        </div>
+        <div class="nav-header">
+          <a href="index.php" class="navbar-brand">Minha Loja</a>
+          </div>
         <div>
-          <ul class="navbar-nav">
-          <li><a href="/cadastro_produto.php">Cadastro de Produtos</a><br />
-          <li><a href="/cadastro_categoria.php">Cadastro de Categorias</a>
+          <ul class="nav navbar-nav">
+          <li><a href="/cadastro_produto.php">Cadastro de Produtos</a></li>
+          <li><a href="/cadastro_categoria.php">Cadastro de Categorias</a></li>
           </ul>
         </div>
       </div>
